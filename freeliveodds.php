509a446957456fb0e0adda63c0c50e5d1a16438f
nixon <nichofernandez1@gmail.com>
2023-04-19 11:27:36 -0600

    correcion en el archivo del plugin

--- freeliveodds.php
+++ freeliveodds.php
@@ -11,7 +11,9 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "\conf.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo SITE_NAME; ?></title>
 
-	<link rel="stylesheet" href="public/css/styles.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/styles.css">
 
 	<!-- Bootstrap core CSS -->
 	<link href="css/bootstrap.css" rel="stylesheet">
